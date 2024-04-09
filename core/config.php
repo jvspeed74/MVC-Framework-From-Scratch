1589205f55957f3e1cfa825e481105e9deacbbae
Jalen Vaughn <javaugh@iu.edu>
2024-04-08 23:38:12 -0400

    Added config file

---
+++ core/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Author: Jalen Vaughn
+ * Date: 4/8/24
+ * File: config.php
+ * Description:
+ */
+
+// todo add mode for production
+
+// Set up error reporting for development
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Base URL
+const BASE_URL = "/I211-Team-Project";
