6f086b4a824ab762f3cf33968413b22d74d61b42
Jalen Vaughn <javaugh@iu.edu>
2024-04-09 12:04:40 -0400

    Added base url

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ require_once "vendor/autoload.php";
 require_once "core/config.php";
 
 $router = new Router();
-//$router->get('/', 'ProductController@index');
-$router->get('/I211-Team-Project/index.php', 'ProductController@index');
+$router->get(BASE_URL . "/index.php", 'ProductController@index');
+$router->get(BASE_URL . "/index.php/product/show", 'ProductController@show');
 
 $dispatcher = new Dispatcher($router);
 $dispatcher->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
