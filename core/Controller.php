ac4d847835c79754af70f997254b23b95a5ded49
jvspeed74 <javaugh@iu.edu>
2024-04-10 14:14:26 -0400

    Added abstract methods

--- core/Controller.php
+++ core/Controller.php
@@ -7,6 +7,16 @@ Date: 4/9/2024
 Description: 
 */
 
-class Controller {
+abstract class Controller {
+    protected object $model;
+    
+    public function __construct() {
+        $this->model = $this->loadModel();
+    }
+    abstract protected function loadModel(): object;
+    
+    public function error(string $message='An error occurred.'): void {
+        ErrorView::render($message);
+    }
     
 }
