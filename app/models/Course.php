b3673668272f0feb09e077fc4ef1491be1e9042e
Jalen Vaughn <javaugh@iu.edu>
2024-04-20 13:55:07 -0400

    Added getters and setters

--- app/models/Course.php
+++ app/models/Course.php
@@ -1,11 +1,98 @@
 <?php
-
 /**
- * Author: Jalen Vaughn
- * Date: 4/20/24
- * File: Course.php
- * Description:
+ * Data Transfer Object for the Courses Table
  */
 class Course {
+    private ?string $courseID = null;
+    private ?string $title = null;
+    private ?string $description = null;
+    private ?string $date = null;
+    private ?string $startTime = null;
+    private ?string $endTime = null;
+    
+    /**
+     * @return string|null
+     */
+    public function getCourseID(): ?string {
+        return $this->courseID;
+    }
+    
+    /**
+     * @param string|null $courseID
+     */
+    public function setCourseID(?string $courseID): void {
+        $this->courseID = $courseID;
+    }
+    
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string {
+        return $this->title;
+    }
+    
+    /**
+     * @param string|null $title
+     */
+    public function setTitle(?string $title): void {
+        $this->title = $title;
+    }
+    
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string {
+        return $this->description;
+    }
+    
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void {
+        $this->description = $description;
+    }
+    
+    /**
+     * @return string|null
+     */
+    public function getDate(): ?string {
+        return $this->date;
+    }
+    
+    /**
+     * @param string|null $date
+     */
+    public function setDate(?string $date): void {
+        $this->date = $date;
+    }
+    
+    /**
+     * @return string|null
+     */
+    public function getStartTime(): ?string {
+        return $this->startTime;
+    }
+    
+    /**
+     * @param string|null $startTime
+     */
+    public function setStartTime(?string $startTime): void {
+        $this->startTime = $startTime;
+    }
+    
+    /**
+     * @return string|null
+     */
+    public function getEndTime(): ?string {
+        return $this->endTime;
+    }
+    
+    /**
+     * @param string|null $endTime
+     */
+    public function setEndTime(?string $endTime): void {
+        $this->endTime = $endTime;
+    }
+    
     
 }
