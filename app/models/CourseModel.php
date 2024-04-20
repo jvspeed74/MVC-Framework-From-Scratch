64a5c09f35ab2ef7201d719e89787d38b8086e9d
Jalen Vaughn <javaugh@iu.edu>
2024-04-20 14:04:07 -0400

    Added fetch methods

--- app/models/CourseModel.php
+++ app/models/CourseModel.php
@@ -7,19 +7,36 @@
  * Description:
  */
 class CourseModel extends Model {
+    protected Database $db;
+    private static ?CourseModel $_instance = null;
+    private string $table = 'Courses';
     
-    /**
-     * @inheritDoc
-     */
-    static public function getInstance() {
-        // TODO: Implement getInstance() method.
+    private function __construct() {
+        parent::__construct();
     }
     
     /**
      * @inheritDoc
      */
-    public function fetchByID(string $id): mixed {
+    static public function getInstance(): CourseModel {
+        if (self::$_instance == null) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function fetchByID(string $id): ?Course {
         // TODO: Implement fetchByID() method.
+        // Request product from DB
+        $sql = "SELECT * FROM $this->table WHERE courseID='$id'";
+        $query = $this->db->query($sql);
+        
+        // Return an instance of a product with provided data
+        return $query->fetch_object(Course::class);
+        
     }
     
     /**
@@ -27,6 +44,21 @@ class CourseModel extends Model {
      */
     public function fetchAll(): array {
         // TODO: Implement fetchAll() method.
+        // Declare SQL
+        $sql = "SELECT * FROM $this->table ORDER BY date";
+        
+        // Query DB for data
+        $query = $this->db->query($sql);
+        
+        // Create product obj from result
+        $results = [];
+        while ($row = $query->fetch_object(Course::class)) {
+            $results[] = $row;
+        }
+        
+        // List of Product objects
+        //todo check error handling
+        return $results;
     }
     
     /**
@@ -34,6 +66,32 @@ class CourseModel extends Model {
      */
     public function fetchBySearch(string $terms): array {
         // TODO: Implement fetchBySearch() method.
+        // separate terms into array
+        $searchTerms = explode(" ", $terms);
+        
+        // Escape each term to prevent SQL injection
+        $escapedTerms = [];
+        foreach ($searchTerms as $term) {
+            $escapedTerms[] = $this->db->realEscapeString($term);
+        }
+        
+        // sql for search
+        $sql = "SELECT * FROM $this->table WHERE title LIKE '%" . array_shift($escapedTerms) . "%'";
+        
+        // pass each term in array
+        foreach ($escapedTerms as $term) {
+            $sql .= " AND title LIKE '%$term%'";
+        }
+        
+        // execute query
+        $query = $this->db->query($sql);
+        
+        // store results in array of Product objects
+        $results = [];
+        while ($row = $query->fetch_object(Course::class)) {
+            $results[] = $row;
+        }
+        return $results;
     }
     
     /**
