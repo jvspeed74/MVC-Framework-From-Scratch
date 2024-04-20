fed6def7d3bd490304c658aae82d1eb35aad1f5f
jvspeed74 <javaugh@iu.edu>
2024-04-20 02:20:12 -0400

    Fixed an issue where CSS and JS weren't loading properly

--- app/views/View.php
+++ app/views/View.php
@@ -33,7 +33,7 @@ abstract class View {
             <!-- Bootstrap icons-->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
             <!-- Core theme CSS (includes Bootstrap)-->
-            <link href="css/styles.css" rel="stylesheet"/>
+            <link href="/I211-Team-Project/public/css/styles.css" rel="stylesheet"/>
         </head>
         <body>
         <!-- Navigation-->
@@ -128,7 +128,7 @@ abstract class View {
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <script src="/I211-Team-Project/public/js/scripts.js"></script>
         </body>
         </html>
         <?php
