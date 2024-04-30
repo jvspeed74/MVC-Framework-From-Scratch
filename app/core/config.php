a11f80851b82839996ee4891cf853ebc73e2fdfa
jvspeed74 <javaugh@iu.edu>
2024-04-30 16:16:55 -0400

    Removed redundant switch statement

--- app/core/config.php
+++ app/core/config.php
@@ -4,22 +4,9 @@
  * Description: Set error reporting, development modes, and base URL of the application.
  */
 
-// Set up error reporting and development modes based on CI_MODE constant
-const CI_MODE = ""; // Possible values: "development", "production"
-
-switch (CI_MODE) {
-    case "development":
-        // Set up error reporting for development
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
-        break;
-    default:
-        // Set up error reporting for production
-        error_reporting(E_ERROR | E_WARNING | E_PARSE);
-        ini_set('display_errors', 0);
-}
-
-// Set error handling configuration
+// Set up error reporting for production
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . "/error.log");
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
@@ -27,9 +14,11 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 // Default handler for uncaught exceptions
 set_exception_handler(['ExceptionHandler', 'handleException']);
 
-// Base URL for the application
-const BASE_URL = "/I211-Team-Project/public/index.php";
 // Base URL used for routing within the application
+const BASE_URL = "/I211-Team-Project/public/index.php";
+
+// Image URL
+const IMG_URL = "/I211-Team-Project/public";
 
 // Set the default time zone to East Coast
 date_default_timezone_set('America/New_York'); // Default time zone for the application
