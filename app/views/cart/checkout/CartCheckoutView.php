ef88b1a549c4418516fffdbb47d1332481d7fdbc
jvspeed74 <javaugh@iu.edu>
2024-04-30 20:52:45 -0400

    Updated TODO

--- app/views/cart/checkout/CartCheckoutView.php
+++ app/views/cart/checkout/CartCheckoutView.php
@@ -2,8 +2,6 @@
 
 /**
  * todo document
- * todo login view needs message stating login to continue
- * todo checkout should only appear if item quantity is above 0
  */
 class CartCheckoutView extends CartView {
     
