6f1b5f77f150dc8d1d278fd7279fc3b53280ecee
jvspeed74 <javaugh@iu.edu>
2024-05-10 22:02:59 -0400

    Added composer autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jalenvaughn/i211-team-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c7ced7a4ee776ec6b21b5d64c4461c23a66c189b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jalenvaughn/i211-team-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c7ced7a4ee776ec6b21b5d64c4461c23a66c189b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
